8e768b9ee7d8672042aef197d1df28684507ffca
EVGENIY-12 <pet3094@gmail.com>
2022-03-06 17:50:47 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+const cardsMarkup = createImgCardsMakup(galleryItems);
+galleryEl.innerHTML = cardsMarkup;
+
+
+function createImgCardsMakup(galleryImages) {
+    return galleryImages.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" 
+            alt="${description}" />
+            </a>`;
+    }).join('');
+};
+
+galleryEl.addEventListener('click', onGalleryImageClick);
+
+function onGalleryImageClick(event) {
+    event.preventDefault();
+
+};
+
+var lightbox = new SimpleLightbox('.gallery a', {'captionsData': 'alt','captionDelay': 250 });
