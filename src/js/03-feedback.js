0833c2ab72adebfb5c908d4064d09504245fbfcf
EVGENIY-12 <pet3094@gmail.com>
2022-03-17 16:43:53 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,18 +4,19 @@ import throttle from 'lodash.throttle';
 const refs = {
     form: document.querySelector('.feedback-form'),
     input: document.querySelector('[type="email"]'),
-    textarea: document.querySelector('form textarea')
+    textarea: document.querySelector('form textarea'),
 }
 
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 refs.form.addEventListener('submit', onFormSubmit);
 
-populateTextarea();
-
 let formData = {};
 
-function onFormInput(evt) {
+populateTextarea();
 
+
+
+function onFormInput(evt) {
     formData[evt.target.name] = evt.target.value;
 
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
@@ -24,45 +25,32 @@ function onFormInput(evt) {
 
 function onFormSubmit(evt) {
     evt.preventDefault();
-    
-    const getItemsMessage = localStorage.getItem("feedback-form-state");
-    const localStorageItems = JSON.parse(getItemsMessage);
 
-    if (localStorageItems) {
-        formData = localStorageItems
-    }
-    
     console.log("formData", formData);
     formData = {};
     
-    evt.currentTarget.reset();
     localStorage.removeItem("feedback-form-state");
-    
-    
+    evt.currentTarget.reset();
+
 }
 
 function populateTextarea() {
-    const saveMessage = localStorage.getItem("feedback-form-state");
-    
-    if (saveMessage) {
-        const localStorageItems = JSON.parse(saveMessage)
-        // console.log('localStorage', localStorageItems);
-
+    const getItemsMessage = localStorage.getItem("feedback-form-state");
+    const localStorageItems = JSON.parse(getItemsMessage);
+   
+    if (localStorageItems) {
+        formData = localStorageItems;
         
-        if (localStorageItems.message) {
-            refs.textarea.value = localStorageItems.message;
-        }
-
-        else {
-            refs.textarea.value = '';
-        }
-
+    }
+    
+    if (localStorageItems) {
+        
         if (localStorageItems.email) {
             refs.input.value = localStorageItems.email;
         }
 
-        else {
-            refs.input.value = '';
+        if (localStorageItems.message) {
+            refs.textarea.value = localStorageItems.message;
         }
   
     }
